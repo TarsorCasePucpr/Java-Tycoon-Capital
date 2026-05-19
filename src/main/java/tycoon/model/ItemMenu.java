4a00a180324a0b01d33e20d085be13ca772e8da2
Gerard Gonzalez <gerard.gonzalez@pucpr.edu.br>
2026-05-18 20:40:31 -0400

    Update:  Criacao do user

--- src/main/java/tycoon/model/ItemMenu.java
+++ src/main/java/tycoon/model/ItemMenu.java
@@ -1,10 +1,13 @@
 package tycoon.model;
 
 public abstract class ItemMenu {
+    //preco de compra nao incrementa com a quantidade que uma pessoa compra
+    // Em todo momento ele mostra a quantidade que voce pode comprar baseado na quantidade de dinheiro que voce tem
     private abstract int preco_compra;
     private abstract int quantidade;
     private abstract int lucro;
     private abstract int velocidade_producao;
+    private abstract int tempo_producao;
     public abstract void comprar();
 
 }
---
+++ src/main/java/tycoon/model/User.java
@@ -0,0 +1,7 @@
+package tycoon.model;
+
+// Keep earning money even if you are offline — maybe implement, faz sentido?
+// Runs the clicking actions for you while you are offline
+public class Managers {
+
+}
