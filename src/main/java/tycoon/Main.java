4dfb2f24909f88a6e3f6f78c612d38833fd9cd91
Gerard Gonzalez <gerard.gonzalez@pucpr.edu.br>
2026-05-18 20:17:17 -0400

    Update:  Ultimos comentarios sobre a implmentacao

--- src/main/java/tycoon/Main.java
+++ src/main/java/tycoon/Main.java
@@ -4,6 +4,8 @@ package tycoon;
 // Implment class rewards it gives multipliers for items
 // New items (classes) for new worlds
 // Angels Investors 
+// Como nos vamos organizar os items desbloqueados e bloqueados e a quantidade de cada item?
+// Multiplicadores tomar en consideracao deixar uma variable para mudar caso o user mude a quantidade pela qual se tem que multiplicar 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Java Tycoon Capital");
