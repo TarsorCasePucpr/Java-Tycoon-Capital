3a9b23f360465b911764c9d4342bb3960af5115b
Gerard Gonzalez <gerard.gonzalez@pucpr.edu.br>
2026-05-18 21:25:54 -0400

    Update:  yes

--- src/main/java/tycoon/model/ItemMenu.java
+++ src/main/java/tycoon/model/ItemMenu.java
@@ -3,10 +3,14 @@ package tycoon.model;
 public abstract class ItemMenu {
     // Preco de compra nao incrementa com a quantidade que uma pessoa compra
     // Em todo momento ele mostra a quantidade que voce pode comprar baseado na quantidade de dinheiro que voce tem
+    // Logica da compra de certa quantidade de items com funciona investigar
     private abstract int preco_compra;
     private abstract int quantidade;
     private abstract int lucro;
     private abstract int velocidade_producao;
     private abstract int tempo_producao;
     public abstract void comprar();
+    public abstract void reset_cambio_mundo();
+    
 }
+
