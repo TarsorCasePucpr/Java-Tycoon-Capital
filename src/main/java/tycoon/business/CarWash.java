6206f6edbe0878115ba2865ccefc69f14177fd60
Kaua Rubbo <kaua.rubbo@pucpr.edu.br>
2026-05-22 22:06:35 -0300

    Add: implementacao de valores e tempos nas classes do business

--- src/main/java/tycoon/business/CarWash.java
+++ src/main/java/tycoon/business/CarWash.java
@@ -1,50 +1,45 @@
 package tycoon.model;
 
+import tycoon.business.ExcecaoSaldoInsuficiente;
 import tycoon.business.ItemMenu;
+import tycoon.business.User;
 
 public class CarWash extends ItemMenu {
 
-    //Construtor CarWash reutiliza o construtor do ItemMenu
-    public CarWash(int preco_compra,int quantidade,int lucro,int velocidade_producao,int tempo_producao){
-        super(preco_compra,quantidade,lucro,velocidade_producao,tempo_producao);
+    private static final int PRECO_COMPRA       = 540;
+    private static final int QUANTIDADE_INICIAL = 0;
+    private static final int LUCRO              = 70;
+    private static final int TEMPO_PRODUCAO     = 6;
+
+    // Construtor sem argumentos — valores já definidos
+    public CarWash() {
+        super(PRECO_COMPRA, QUANTIDADE_INICIAL, LUCRO, TEMPO_PRODUCAO);
     }
 
     @Override
-    //Metodo compra do item com ExcecaoSaldoInsuficiente
     public void comprar(User user) throws ExcecaoSaldoInsuficiente {
-
-        //Verificação de saldo insuficiente -> ExcecaoSaldoInsuficiente
-        if(user.globalmoneyquantity < this.preco_compra){
+        if (user.globalmoneyquantity < this.preco_compra) {
             throw new ExcecaoSaldoInsuficiente("Saldo insuficiente!");
         }
 
-        //Calculo da quantidade que pode ser comprada do item (O calculo tem que ser feito o tempo todo em bucle)
-        int quantidadeCompravel = (int)(user.globalmoneyquantity / this.preco_compra);
-
-        //Calculo de quantos é o custo de todos os item (O calculo tem que ser feito o tempo todo em bucle)
+        int quantidadeCompravel = (int) (user.globalmoneyquantity / this.preco_compra);
         long custoTotal = (long) quantidadeCompravel * this.preco_compra;
 
-        //Subtraímos o custo da compra dos items do valor geral que o user têm
         user.globalmoneyquantity -= custoTotal;
-
-        //Atualizamos a quantidade de items que o user possui
         this.quantidade += quantidadeCompravel;
 
-        //Print :-) ------- (Mudar quando implementarmos a intereface) -------
-        System.out.println("Comprou "+ quantidadeCompravel+ " de lava-rápidos");
-
-        //Print :-) ------- (Mudar quando implementarmos a intereface) -------
-        System.out.println("Agora possui "+ this.quantidade);
+        System.out.println("Comprou " + quantidadeCompravel + " Lava-rápidos");
+        System.out.println("Agora possui " + this.quantidade);
     }
 
     @Override
-    //Metodo receber lucro do item (Atauliza o dinheiro do usuario sumando a quantidade gerada do item em um periodo de tempo ate o click do User ou ---Manager---)
-    public void receberLucro(User user){
-
-        // Atualizar o dinheiro do usuario sumando a quantidade gerada do item
+    public void receberLucro(User user) {
         user.globalmoneyquantity += this.getLucro();
+        System.out.println("Recebeu lucro de: " + this.getLucro());
+    }
 
-        //Print :-) ------- (Mudar quando implementarmos a intereface) -------
-        System.out.println("Recebeu lucro de: "+ this.getLucro());
+    @Override
+    public void reset_cambio_mundo() {
+        this.quantidade = 0;
     }
 }
--- src/main/java/tycoon/business/ItemMenu.java
+++ src/main/java/tycoon/business/ItemMenu.java
@@ -2,42 +2,24 @@ package tycoon.business;
 
 public abstract class ItemMenu {
     
-    // Preco de compra nao incrementa com a quantidade que uma pessoa compra
-    // Em todo momento ele mostra a quantidade que voce pode comprar baseado na quantidade de dinheiro que voce tem
-    // Logica da compra de certa quantidade de items com funciona investigar
-    
     protected int preco_compra;
     protected int quantidade;
     protected int lucro;
-    protected int velocidade_producao;
     protected int tempo_producao;
 
-    public ItemMenu(int preco_compra,int quantidade,int lucro,int velocidade_producao,int tempo_producao){
+    public ItemMenu(int preco_compra, int quantidade, int lucro, int tempo_producao){
         this.preco_compra = preco_compra;
         this.quantidade = quantidade;
         this.lucro = lucro;
-        this.velocidade_producao = velocidade_producao;
         this.tempo_producao = tempo_producao;
     }
 
-    public int getPrecoCompra(){
-        return this.preco_compra;
-    }
+    public int getPrecoCompra(){ return this.preco_compra; }
+    public int getQuantidade(){ return this.quantidade; }
+    public int getLucro(){ return this.lucro * this.quantidade; }
+    public int getTempo(){ return this.tempo_producao; }
 
-    public int getQuantidade(){
-        return this.quantidade;
-    }
-
-    public int getLucro(){
-        return this.lucro * this.quantidade;
-    }
-    public int velocidadeProducao(){
-        return this.velocidade_producao;
-    }
-    public int tempo_producao(){
-        return this.tempo_producao;
-    }
-    public abstract void comprar();
+    public abstract void comprar(User user) throws ExcecaoSaldoInsuficiente;
+    public abstract void receberLucro(User user);
     public abstract void reset_cambio_mundo();
-    
 }
