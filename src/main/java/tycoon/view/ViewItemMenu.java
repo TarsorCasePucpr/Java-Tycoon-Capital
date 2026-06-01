d4b6822dedc80307e7106fabed5cabc6a8cb60f1
Bear <mark.oliveira2511@gmail.com>
2026-06-01 18:54:12 -0300

    Update ViewItemMenu.java
    Correcao de sintaxe

--- src/main/java/tycoon/view/ViewItemMenu.java
+++ src/main/java/tycoon/view/ViewItemMenu.java
@@ -1,13 +1,21 @@
 package tycoon.view;
 
-import javax.swing.*;
 import java.awt.Dimension;
+import javax.swing.*;
+import tycoon.Game;
+import tycoon.model.User;
 
 public class ViewItemMenu {
 
     private JFrame janela;
 
     public void show() {
+        User user = new User();
+        Game game = new Game(user);
+        game.start();
+
+        janela = new JFrame("Java Tycoon Capital");
+        janela.setSize(300, 150);
         janela = new JFrame("Java Tycoon Capital");
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -18,22 +26,22 @@ public class ViewItemMenu {
         Dimension dimensao = label.getPreferredSize();
         label.setBounds(300 / 2, 150 / 2, dimensao.width, dimensao.height);
 
-        /Item1
-        //Vai variar baseado na quantidade que o user pode comprar isso vai ter que mudar o tempo todo (Buy x1).
+        // Item1
+        // Vai variar baseado na quantidade que o user pode comprar isso vai ter que
+        // mudar o tempo todo (Buy x1).
         JButton botao1 = new JButton("Buy x1");
         int larguraBotao = 90;
         int alturaBotao = 30;
         int x = (janela.getWidth() - larguraBotao) / 2;
         int y = (janela.getHeight() - alturaBotao) / 2;
         botao1.setBounds(
-            x, y,
-            larguraBotao,
-            alturaBotao
-        );
+                x, y,
+                larguraBotao,
+                alturaBotao);
         painel.add(botao1);
 
-        /Item1
-        JButton botao1 = new JButton("Fechar");
+        // Item1
+        JButton botaoFechar = new JButton("Fechar");
 
         janela.add(painel);
         janela.setSize(300, 150);
@@ -44,5 +52,3 @@ public class ViewItemMenu {
         SwingUtilities.invokeLater(() -> new ViewItemMenu().show());
     }
 }
-
-
